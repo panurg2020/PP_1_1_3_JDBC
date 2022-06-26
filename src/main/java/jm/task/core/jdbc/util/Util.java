0ebc3ce380534ade50a387a6cbef1b4dfa7b2570
panurg2020 <Jklgfyr1>
2022-06-26 18:34:55 +0300

    подключение БД в Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "Jklgfyr1";
+    public static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    public static Statement statement;
+    public static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    static {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
 }
