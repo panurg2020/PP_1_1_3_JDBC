3a069900690a391926da77f2ba27c039b187d0a6
panurg2020 <Jklgfyr1>
2022-06-30 21:41:27 +0300

    подключение БД в Util

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private static final UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 55);
+        userService.saveUser("Петр", "Кравец", (byte) 34);
+        userService.saveUser("Николай", "Борисов", (byte) 19);
+        userService.saveUser("Сеня", "Лютый", (byte) 22);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
