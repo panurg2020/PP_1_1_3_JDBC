8d8fb70f264065f056792e44dbc0d8d0f5b9e7d8
panurg2020 <Jklgfyr1>
2022-06-30 21:39:34 +0300

    подключение БД в Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,29 +1,37 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.*;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import java.util.Properties;
 
 public class Util {
     public static final String USER_NAME = "root";
     public static final String PASSWORD = "Jklgfyr1";
-    public static final String URL = "jdbc:mysql://localhost:3306/mysql";
-    public static Statement statement;
-    public static Connection connection;
+    public static final String URL = "jdbc:mysql://localhost:3306/users";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static SessionFactory sessionFactory;
 
-    static {
+    public static SessionFactory getConnection() {
         try {
-            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
-        } catch (SQLException e) {
+            Properties prop = new Properties();
+            prop.setProperty("hibernate.connection.driver_class", DRIVER);
+            prop.setProperty("hibernate.connection.url", URL);
+            prop.setProperty("hibernate.connection.username", USER_NAME);
+            prop.setProperty("hibernate.connection.password", PASSWORD);
+            prop.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+            prop.setProperty("hibernate.hbm2ddl.auto", "update");
+
+            Configuration configuration = new Configuration();
+            configuration.setProperties(prop);
+            configuration.addAnnotatedClass(User.class);
+            sessionFactory = configuration.buildSessionFactory();
+
+
+        } catch (HibernateException e) {
             e.printStackTrace();
-            throw new RuntimeException();
         }
-    }
-
-    static {
-        try {
-            statement = connection.createStatement();
-        } catch (SQLException e1) {
-            e1.printStackTrace();
-            throw new RuntimeException();
-        }
+        return sessionFactory;
     }
 }
